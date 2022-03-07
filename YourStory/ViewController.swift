cd5dfb1512e92a9c8cde56b7d8b54358a08e3476
rukiyesenturk <rukiyeyagmur28@gmail.com>
2022-03-07 11:57:25 +0300

    Initial Commit

---
+++ YourStory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YourStory
+//
+//  Created by Macbook on 7.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
