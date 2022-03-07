8cc86b0e122f9bdb9832fed0255dc744cce8506b
rukiyesenturk <rukiyeyagmur28@gmail.com>
2022-03-07 13:32:13 +0300

    project completed

---
+++ YourStory/Controller/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  YourStory
+//
+//  Created by Macbook on 7.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    var storyBrain = StoryBrain()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        updateUI()
+    }
+
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let userChoice = sender.currentTitle!
+        storyBrain.nextStory(userChoice)
+        updateUI()
+    }
+    func updateUI(){
+        storyLabel.text = storyBrain.getStoryText()
+        choice1Button.setTitle(storyBrain.getChoice1Text(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2Text(), for: .normal)
+    }
+}
+
---
+++ YourStory/Model/Story.swift
@@ -0,0 +1,16 @@
+//
+//  Story.swift
+//  YourStory
+//
+//  Created by Macbook on 7.03.2022.
+//
+
+import Foundation
+struct Story{
+    let title : String
+    let choice1 : String
+    let choice1Destination : Int
+    let choice2 : String
+    let choice2Destination : Int
+
+}
